c12a7996fbd57c51deb51199f66ab7a2bd3a6bdc
Keabetswe <Keabetswe@DESKTOP-L9QBH6F>
2025-09-06 15:47:43 +0200

    Added a Logout button that takes you straight to the login page after clicking it

--- Dashboard.xaml.cs
+++ Dashboard.xaml.cs
@@ -60,6 +60,14 @@ namespace ClaimDesk
             {
                 main.MainContent.Content = new Settings(); // Navigate to Profile page
             }
+            private void ApproveClaims_Click(object sender, RoutedEventArgs e)
+        {
+            // Navigate to ApproveClaims page
+            if (Application.Current.MainWindow is MainWindow mainWindow)
+            {
+                mainWindow.MainContent.Content = new ApproveClaims();
+            }
         }
+    }
     }
 }
